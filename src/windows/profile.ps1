deabbbc393cbd3f593db687c285c5bc168765377
Val Hsu <vesper.val@qq.com>
2023-03-17 16:43:11 +0800

    add exit shortcut ctrl-d, reopen history search forward

--- src/windows/profile.ps1
+++ src/windows/profile.ps1
@@ -8,6 +8,9 @@ Function gcmt { git commit }
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
+# Exit with ctrl-d
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
 # Autocompletion for arrow keys
-# Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-# Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
